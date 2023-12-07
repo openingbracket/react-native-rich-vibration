af80e558b87ce3167ce83a49bd0fe7e5397a5dde
shafayeatsumit <shafayeatkabir@gmail.com>
2023-12-07 15:24:30 +0600

    chor:more clean up

--- example/App.tsx
+++ example/App.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
-import * as ReactNativeRichVibration from 'react-native-rich-vibration';
+import * as ReactNativeRichVibration from "react-native-rich-vibration";
 
 export default function App() {
   return (
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
